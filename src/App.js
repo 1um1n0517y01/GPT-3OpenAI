f0983d67833edee361e3af0cfdc8463bb08d9009
Milos Ilic <milosilic89@outlook.com>
2023-01-18 14:13:18 +0100

    Fixed typos

--- src/App.js
+++ src/App.js
@@ -11,7 +11,21 @@ import {
 import { CTA, Brand, Navbar } from './components';
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
